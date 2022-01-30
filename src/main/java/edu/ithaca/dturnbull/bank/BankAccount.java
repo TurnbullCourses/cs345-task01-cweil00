cf8936e7da70ad2856c8490563b78d422805bb50
iyayi0106 <iyayi0106@gmail.com>
2022-01-30 00:28:23 -0500

    'isEmailValid and withdraw methods pass tests'

--- src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
+++ src/main/java/edu/ithaca/dturnbull/bank/BankAccount.java
@@ -1,4 +1,4 @@
-package edu.ithaca.dturnbull.bank;
+package edu.ithaca.dturnbull.bank;;
 
 public class BankAccount {
 
@@ -29,13 +29,13 @@ public class BankAccount {
     /**
      * @post reduces the balance by amount if amount is non-negative and smaller than balance
      */
-    public void withdraw (double amount) throws InsufficientFundsException{
-        if (amount <= balance){
+    public void withdraw (double amount) throws InsufficientFundsException, IllegalArgumentException{
+        if (amount < 0)
+            throw new IllegalArgumentException("Cannot withdraw negative amount");
+        else if (amount <= balance)
             balance -= amount;
-        }
-        else {
+        else
             throw new InsufficientFundsException("Not enough money");
-        }
     }
 
 
@@ -43,7 +43,69 @@ public class BankAccount {
         if (email.indexOf('@') == -1){
             return false;
         }
+
         else {
+            String validChars = "abcdefghijklmnopqrstuvwxyz1234567890"; // all valid chars
+            String specialChars = "_-.";
+
+            String[] splitEmail = email.split("@"); // separate prefix and domain into diff arrays
+            String prefix = splitEmail[0];
+            String domain = splitEmail[1];
+            prefix = prefix.toLowerCase(); // normalizing prefix
+            domain = domain.toLowerCase(); // normalizing domain
+
+            // first char of prefix must be in alphabet
+            if (!validChars.substring(0, 26).contains(String.valueOf(prefix.charAt(0)))){
+                return false;
+            }
+
+            // last char of prefix must be in validChars
+            if (!validChars.contains(String.valueOf(prefix.charAt(prefix.length()-1)))){
+                return false;
+            }
+            
+            for (int i = 1; i < prefix.length(); i++){
+                String tempChar = String.valueOf(prefix.charAt(i));
+                
+                // is rest of prefix valid
+                if (!validChars.contains(tempChar) && !specialChars.contains(tempChar)){ 
+                    return false;
+                }
+                else if (tempChar.equals(".") || tempChar.equals("_") || tempChar.equals("-")){
+                    
+                    // no consecutive symbols
+                    if (!validChars.contains(String.valueOf(prefix.charAt(i+1)))){ 
+                        return false;
+                    }
+                }
+            }
+            // does domain have '.'
+            if (domain.indexOf('.') == -1){ 
+                return false;
+            }
+            else{
+                String[] domainSplit = domain.split("\\."); // split domain into before and after '.'
+                String domain1 = domainSplit[0];
+                String domain2 = domainSplit[1];
+                // make sure the end has 2 or more characters
+                if (domain2.length() < 2){ 
+                    return false;
+                }
+                // validation of domain's first part
+                for (int i = 0; i < domain1.length(); i++){
+                    String tempChar = String.valueOf(domain1.charAt(i));
+                    if (!validChars.contains(tempChar) && !tempChar.equals("-")){
+                        return false;
+                    }
+                }
+                // validation of domain's second part
+                for (int i = 0; i < domain2.length(); i++){
+                    String tempChar = String.valueOf(domain2.charAt(i));
+                    if (!validChars.contains(tempChar) && !tempChar.equals("-")){
+                        return false;
+                    }
+                }
+            }
             return true;
         }
     }
